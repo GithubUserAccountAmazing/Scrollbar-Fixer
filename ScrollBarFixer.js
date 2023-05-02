250022bfb0c655dea31d3b535d65635892b9ad28
Rich K. MLS <105183376+RichKMLS@users.noreply.github.com>
2023-05-01 22:05:03 -0500

    check for disabled scrollbars before running
    This commit adds a check for disabled scrollbars on the document and body elements before running the rest of the script. This prevents the script from interfering with websites that intentionally hide scrollbars for design or functionality reasons. The check is performed after a 1 second timeout to allow any other scripts that may modify scrollbars to run first.

--- ScrollBarFixer.js
+++ ScrollBarFixer.js
@@ -9,10 +9,8 @@
 
 (function() {
     'use strict';
-
     // Wait for 1 seconds before executing the rest of the script
     setTimeout(function() {
-
         // Check if there are any scrollbars that are disabled by CSS or JS
         var hasDisabledScrollbars = false;
         [document.documentElement, document.body].forEach(function(el) {
@@ -22,33 +20,27 @@
             }
             return undefined; // Explicitly return undefined to avoid assignment
         });
-
         // If there are no disabled scrollbars, exit the script
         if (!hasDisabledScrollbars) {
             return;
         }
-
         // Retrieve the document and body elements from the DOM
         var [doc, body] = [document.documentElement, document.body];
-
         // Ensure that both elements display scrollbars only when necessary
         [doc, body].forEach(function(el) {
             el.style.overflow = 'auto';
             return undefined; // Explicitly return undefined to avoid assignment
         });
-
         // Override any CSS rules that modify scrollbars
         Object.defineProperty(document, 'styleSheets', {
             get: () => [],
             configurable: false
         });
-
         // Return the current vertical scroll position of the document element
         Object.defineProperty(window, 'scrollY', {
             get: () => doc.scrollTop,
             configurable: false
         });
-
         // Block any scripts that attempt to scroll the window or the elements
         ['scrollTo', 'scrollTop'].forEach(function(prop) {
             [window, doc, body].forEach(function(obj) {
@@ -61,5 +53,4 @@
             return undefined; // Explicitly return undefined to avoid assignment
         });
     }, 1000); // 1 seconds in milliseconds
-
 })();
